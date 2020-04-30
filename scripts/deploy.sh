b3d4bc8e29e7c78036c6f32c74b5bc8a05e53162
Kosta Korenkov <kosta@whoot.me>
2020-04-30 13:07:42 +0300

    copy favicon

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,4 +4,6 @@
 yarn build-mainnet
 aws s3 sync ./build_webpack s3://safe.leapdao.org/app  --acl public-read
 aws s3 cp ./build_webpack/index.html s3://safe.leapdao.org  --acl public-read
+aws s3 cp ./build_webpack/leap-safe-32x32.png s3://safe.leapdao.org  --acl public-read
+aws s3 cp ./build_webpack/leap-safe-196x196.png s3://safe.leapdao.org  --acl public-read
 aws cloudfront create-invalidation --distribution-id EFJWIP910NMBK --paths '/*'
