fd005a8280d8165edc58562be7d920f09370bc97
Kosta Korenkov <kosta@whoot.me>
2020-05-01 16:32:24 +0300

    fix: use checksummed address for DAI in config

--- src/config/development-mainnet.js
+++ src/config/development-mainnet.js
@@ -6,8 +6,8 @@ const devMainnetConfig = {
   ...devConfig,
   [TX_SERVICE_HOST]: 'https://safe-transaction.mainnet.staging.gnosisdev.com/api/v1/',
   [RELAY_API_URL]: 'https://safe-relay.mainnet.staging.gnosisdev.com/api/v1/',
-  [BOUNTY_PAYOUT_ADDR]: '0x572d03fd45e85d5ca0bcd3679c99000d23a6b8f1',
-  [BOUNTY_TOKEN_ADDR]: '0x6b175474e89094c44da98b954eedeac495271d0f' // DAI
+  [BOUNTY_PAYOUT_ADDR]: '0x572d03FD45E85d5ca0BCd3679c99000D23A6b8f1',
+  [BOUNTY_TOKEN_ADDR]: '0x6B175474E89094C44Da98b954EedeAC495271d0F' // DAI
 }
 
 export default devMainnetConfig
